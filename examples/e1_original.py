e30d5058b412dbeb7a461b887326728c1e9f27f1
sergio <sergio.leon.gaixas@gmail.com>
2025-05-15 12:32:54 +0200

    Examples 0-2

---
+++ examples/e1_original.py
@@ -0,0 +1,35 @@
+#***Comments***
+#This is a comment
+    #This is also a comment although with some tabs before
+
+'This is a string without any use'
+    'Unlike comments, a string cannot simply be idented'
+
+'''This is a multiline string
+but not stored anywhere, so it's used comment most of the time'''
+    '''This is a multiline string
+but this one is wrongly idented
+but not stored anywhere, so it's used comment most of the time'''
+
+
+
+
+
+#Too many white spaces here, format should be corrected
+print('This will print in console')
+
+#***Code blocks and identation***
+if True:
+                print("Why so many tabs here")
+                print("This should be reformated")
+if True:
+    print("Ok, only one tab here")
+                print("But this one still has too many tabs")
+
+if True : {
+    print("does this work");print("Same as Java or C?")
+}
+
+
+
+print("The end")
